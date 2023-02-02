6e623e729186cca9475b8fd3bdac6357b209ee53
rowlul <96617295+rowlul@users.noreply.github.com>
2023-02-02 18:57:55 +0200

    Add SPA project

---
+++ Nomadex.SPA/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapFallbackToFile("index.html");
+
+app.Run();
