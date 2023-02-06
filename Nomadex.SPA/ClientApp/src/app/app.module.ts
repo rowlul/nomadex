533d7092d69521120772fe944115bd82bef46539
rowlul <96617295+rowlul@users.noreply.github.com>
2023-02-06 21:39:14 +0200

    Implement routing and lazy loading

--- Nomadex.SPA/ClientApp/src/app/app.module.ts
+++ Nomadex.SPA/ClientApp/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLayoutModule } from './layouts/home-layout/home-layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HomeLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
