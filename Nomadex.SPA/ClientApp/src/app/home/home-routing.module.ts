c956d9058fa8e863687e3d2462d305876232c4cb
rowlul <96617295+rowlul@users.noreply.github.com>
2023-02-09 17:27:44 +0200

    Add routes for pages in home module

--- Nomadex.SPA/ClientApp/src/app/home/home-routing.module.ts
+++ Nomadex.SPA/ClientApp/src/app/home/home-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent as HomePageComponent } from './pages/home/home.component';
+import { HotelsComponent } from './pages/hotels/hotels.component';
+import { OffersComponent } from './pages/offers/offers.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
+import { RoomsComponent } from './pages/rooms/rooms.component';
 
 const routes: Routes = [
   {
@@ -11,6 +17,30 @@ const routes: Routes = [
       {
         path: '',
         component: HomePageComponent,
+      },
+      {
+        path: 'hotels',
+        component: HotelsComponent,
+      },
+      {
+        path: 'rooms',
+        component: RoomsComponent,
+      },
+      {
+        path: 'offers',
+        component: OffersComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'reservations',
+        component: ReservationsComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
       },
     ],
   },
