daf9be9ac5d2e1d8b7300dc5165af161c3c06c20
rowlul <96617295+rowlul@users.noreply.github.com>
2023-02-09 07:29:43 +0200

    Add date inputs with current/min date

--- Nomadex.SPA/ClientApp/src/app/home/pages/home/home.component.ts
+++ Nomadex.SPA/ClientApp/src/app/home/pages/home/home.component.ts
@@ -1,7 +1,11 @@
+import { formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  currentDate = Date.now();
+  formattedCurrentDate = formatDate(this.currentDate, 'yyyy-MM-dd', 'en-US');
+}
